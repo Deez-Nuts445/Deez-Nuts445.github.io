97a473d9dfc4bbbd8e2885cb947ac805270f4b79
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2022-10-24 20:18:27 +0800

    Update sss.lua

--- sss.lua
+++ sss.lua
@@ -3,40 +3,34 @@ local Window = Library.CreateLib("Chatlines", "Midnight")
 local me = Window:NewTab("Chatlines")
 local mee = me:NewSection("Maple Hospital Chat lines")
 
+function chat(msg,rec)
+local args = {
+    [1] = msg,
+    [2] = rec
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+end	
+
 -- Chatline stuff --
 
 mee:NewButton("Maple Hospital Welcome message","Makes you chat the line",function()
-local args = {
-    [1] = "We are pleased to welcome you to Maple Hospital, where lives are saved every day! How may I help you?",
-    [2] = "All"
-}
-
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+chat("We are pleased to welcome you to Maple Hospital, where lives are saved every day! How may I help you?","All")
+end)
+mee:NewButton("Privacy Matters.","Makes you chat the line",function()
+chat("Privacy Matters.","All")
+wait(1)
+chat("Close the curtains before and during treatment.","All")		
 end)
 local dee = me:NewSection("Other Chat Lines")
 dee:NewButton("What is happening over here?","Makes you chat the line",function()
-local args = {
-    [1] = "What is happening over here?",
-    [2] = "All"
-}
-
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+chat("What is happening over here?","All")
 end)
 dee:NewButton("I give up, I dont get paid enough to do this!","Makes you chat the line",function()
-local args = {
-    [1] = "I give up, I dont get paid enough to do this!",
-    [2] = "All"
-}
-
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+chat("I give up, I dont get paid enough to do this!","All")
 end)
 dee:NewButton("Just checking in...","Makes you chat the line",function()
-local args = {
-    [1] = "Just checking in...",
-    [2] = "All"
-}
-
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+chat("Just checking in...","All")
 end)
 
 local I_Was_Running_Far_Away = Window:NewTab("Chat")
@@ -56,6 +50,6 @@ end)
 
 local Tab = Window:NewTab("Settings")
 local UI = Tab:NewSection("UI Settings")
-UI:NewKeybind("Toggle UI", "Toggles UI", Enum.KeyCode.Home, function()
+UI:NewKeybind("Toggle UI", "Toggles UI", Enum.KeyCode.Z, function()
 	Library:ToggleUI()
 end)
