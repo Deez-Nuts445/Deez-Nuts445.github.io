8e47959d4b4d7dd134910b483b71b9c1d0db3982
Deez-Nuts445 <deeznuts631101@gmail.com>
2023-10-29 11:41:16 +0800

    some stuff

--- script.js
+++ script.js
@@ -16,18 +16,15 @@ function e() {
 function f() {
     window.open("https://is.gd/NRpCZF")
 }
-function g() {
-    window.open("https://deez-nuts445.github.io/redirectr.html")
-}
+// function g() {
+//     window.open("https://deez-nuts445.github.io/redirectr.html")
+// }
 function h() {
     window.open("https://hub.docker.com/u/thatonenoob")
 }
 function i() {
     window.open("https://www.patreon.com/D3xScripts")
 }
-function j() {
-    navigator.clipboard.writeText('<link rel="stylesheet" href="https://rawcdn.githack.com/Deez-Nuts445/card.css/fb48ec3aadc780c613ce383c80578e1ebe6d8f10/card.css">')
-}
-function k(){
-    window.open('https://deez-nuts445.github.io/card.css/index.html')
-}
+// function k(){
+//     window.open('https://deez-nuts445.github.io/card.css/index.html')
+// }
