4a5be0cac3f1590b54f44ebe80b120bf30260c55
Herman <deeznuts631101@gmail.com>
2023-12-29 21:15:24 +0800

    fuck me google

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-// function a() {
-// window.open("https://poe.com/LuaBot");
-// };
-// function b() {
-// window.open("https://www.buymeacoffee.com/deeznuts445");
-// };
-// function c() {
-//     window.open("https://kick.com/DeezNuts455");
-// }; 
-// function d() {
-//     window.open("https://user.rblx.name/herman_484")
-// };
-// function e() {
-//    window.open("https://chng.it/4kbMYG5VqT") 
-// };
-// function f() {
-//     window.open("https://is.gd/NRpCZF")
+function a() {
+window.open("https://poe.com/LuaBot");
+};
+function b() {
+window.open("https://www.buymeacoffee.com/deeznuts445");
+};
+function c() {
+    window.open("https://kick.com/DeezNuts455");
+}; 
+function d() {
+    window.open("https://user.rblx.name/herman_484")
+};
+function e() {
+   window.open("https://chng.it/4kbMYG5VqT") 
+};
+function f() {
+    window.open("https://is.gd/NRpCZF")
+}
+// function g() {
+//     window.open("https://deez-nuts445.github.io/redirectr.html")
 // }
-// // function g() {
-// //     window.open("https://deez-nuts445.github.io/redirectr.html")
-// // }
-// function h() {
-//     window.open("https://hub.docker.com/u/thatonenoob")
+function h() {
+    window.open("https://hub.docker.com/u/thatonenoob")
+}
+function i() {
+    window.open("https://www.patreon.com/D3xScripts")
+}
+// function k(){
+//     window.open('https://deez-nuts445.github.io/card.css/index.html')
 // }
-// function i() {
-//     window.open("https://www.patreon.com/D3xScripts")
-// }
-// // function k(){
-// //     window.open('https://deez-nuts445.github.io/card.css/index.html')
-// // }
